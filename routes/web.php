12a4be497f612b531274f6a9768f9acdbee0dd63
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2024-03-19 16:30:53 +0700

    Create Dashboards for Users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('admin/dashboard', [AdminDashboardController::class, 'index'])->middleware('auth')->name('admin.dashboard');
+
 require __DIR__.'/auth.php';
+
