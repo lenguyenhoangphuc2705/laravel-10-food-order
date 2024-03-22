686feb52d62d930ab5e0f687bdc1faa1bf0dbba2
Phucle2705 <0306201275@caothang.edu.vn>
2024-03-22 17:27:29 +0700

    Admin - Working on Admin Password Update Feature

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ProfilePasswordUpdateRequest;
 use App\Http\Requests\Admin\ProfileUpdateRequest;
 use Auth;
 use Illuminate\Contracts\View\View;
@@ -20,12 +21,23 @@ class ProfileController extends Controller
         $user = Auth::user();
         $user->name = $request->name;
         $user->email = $request->email;
-       
+
         $user->save();
 
         toastr('Updated Successfully!', 'success');
 
-
         return redirect()->back();
+    }
+
+    function updatePassword(ProfilePasswordUpdateRequest $request): RedirectResponse{
+
+        $user = Auth::user();
+        $user->password = bcrypt($request->password);
+        $user->save();
+        toastr()->success('Password Updated Successfully');
+
+
+      return redirect()->back();
+
     }
 }
