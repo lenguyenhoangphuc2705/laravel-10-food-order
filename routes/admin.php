b37cef1f463775d8c7583042c3a3ad65d452fda4
Phucle2705 <0306201275@caothang.edu.vn>
2024-03-22 13:30:48 +0700

    Admin - Working on Admin Profile Update Feature (Part - 1)

--- routes/admin.php
+++ routes/admin.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\Admin\AdminAuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\ProfileController;
+
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
 
-          
 
     Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+    /* Profile Route*/
+    Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
 });
