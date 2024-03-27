699698461827de599e52449fdbc74d879b55e193
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2024-03-27 12:12:30 +0700

    Home Slider- Install and Setup of Laravel Yajrabox Datatables

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="{{asset('admin/assets/modules/fontawesome/css/all.min.css')}}">
 
   <link rel="stylesheet" href="{{asset('admin/assets/css/toastr.min.css')}}">
+  <link rel="stylesheet" href="//cdn.datatables.net/2.0.3/css/dataTables.dataTables.min.css">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{asset('admin/assets/css/style.css')}}">
@@ -58,7 +59,7 @@
 
   <script src="{{ asset('admin/assets/js/toastr.min.js') }}"></script>
   <script src="{{ asset('admin/assets/modules/upload-preview/assets/js/jquery.uploadPreview.min.js')}}"></script>
-
+  <script src="//cdn.datatables.net/2.0.3/js/dataTables.min.js"></script>
   <!-- Template JS File -->
   <script src="{{asset('admin/assets/js/scripts.js')}}"></script>
   <script src="{{asset('admin/assets/js/custom.js')}}"></script>
