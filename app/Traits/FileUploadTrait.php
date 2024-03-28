ef8c5faa8e0b44d9cc921cf805ada407bc01a3bd
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2024-03-28 16:20:47 +0700

    Home Slider- Update Slider Feature (Part - 3)

--- app/Traits/FileUploadTrait.php
+++ app/Traits/FileUploadTrait.php
@@ -3,18 +3,25 @@
 namespace App\Traits;
 
 use Illuminate\Http\Request;
-
+use File;
 trait FileUploadTrait {
 
-    function uploadImage(Request $request, $inputName, $path = "/uploads"){
+    function uploadImage(Request $request, $inputName, $oldPath = Null, $path = "/uploads"){
         if ($request->hasFile($inputName)) {
+            
             $image = $request->{$inputName};
             $ext = $image->getClientOriginalExtension();
             $imageName = 'media_'.uniqid().'.'.$ext;
 
             $image->move(public_path($path), $imageName);
 
+            //delete previous file if exits
+            if($oldPath && File::exists(public_path($oldPath))){
+                File::delete(public_path($oldPath));
+            }
+
             return $path.'/'.$imageName;
+
         }
         return NULL;
     }
