dc15199344721b40e2d2d55dfde801a3db455ef7
Phucle2705 <0306201275@caothang.edu.vn>
2024-03-31 17:46:57 +0700

    Why Choose Us - Working with Update Feature

--- app/Http/Controllers/Admin/WhyChooseUsController.php
+++ app/Http/Controllers/Admin/WhyChooseUsController.php
@@ -45,28 +45,28 @@ class WhyChooseUsController extends Controller
         return to_route('admin.why-choose-us.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        $whyChooseUs = WhyChooseUs::findOrFail($id);
+        return view('admin.why-choose-us.edit', compact('whyChooseUs'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(WhyChooseUsCreateRequest $request, string $id) : RedirectResponse
     {
-        //
+        $whyChooseUs = WhyChooseUs::findOrFail($id);
+        $whyChooseUs->update($request->validated());
+
+        toastr()->success('Created Successfully');
+
+        return to_route('admin.why-choose-us.index');
     }
     public function updateTitle(Request $request) {
         $request->validate([
