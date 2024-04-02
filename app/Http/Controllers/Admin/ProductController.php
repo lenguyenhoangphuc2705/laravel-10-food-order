5a057a0c28c4be6fd9e6e0d44693c691927a009b
Phucle2705 <0306201275@caothang.edu.vn>
2024-04-02 15:44:17 +0700

    Product - Working with Delete Feature

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -9,9 +9,12 @@ use App\Http\Requests\Admin\ProductUpdateRequest;
 use App\Models\Category;
 use App\Traits\FileUploadTrait;
 use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\View\View;
+use Response;
 
 class ProductController extends Controller
 {
@@ -19,7 +22,7 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(ProductDataTable $dataTable)
+    public function index(ProductDataTable $dataTable) : View|JsonResponse
     {
         return $dataTable->render('admin.product.index');
     }
@@ -36,7 +39,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(ProductCreateRequest $request)
+    public function store(ProductCreateRequest $request) :RedirectResponse
     {
 /** Handle image file */
 $imagePath = $this->uploadImage($request, 'image');
@@ -84,7 +87,7 @@ return to_route('admin.product.index');
     /**
      * Update the specified resource in storage.
      */
-    public function update(ProductUpdateRequest $request, string $id)
+    public function update(ProductUpdateRequest $request, string $id): RedirectResponse
     {
                 $product = Product::findOrFail($id);
 
@@ -114,8 +117,16 @@ return to_route('admin.product.index');
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id) : HttpResponse
     {
-        //
+        try{
+            $product = Product::findOrFail($id);
+            $this->removeImage($product->thumb_image);
+            $product->delete();
+
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+        }catch(\Exception $e){
+            return response(['status' => 'error', 'message' => 'something went wrong!']);
+        }
     }
 }
