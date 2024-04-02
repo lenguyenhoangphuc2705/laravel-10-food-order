9241e27640bad9d2db0324c2577bb29ed6a9c0be
Phucle2705 <0306201275@caothang.edu.vn>
2024-04-02 09:29:23 +0700

    Product - Creating Necessary Files and Designs

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\CategoryController;
@@ -30,4 +31,6 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
      /* Product  Category Route*/
 Route::resource('category', CategoryController::class);
 
+  /* Product  Category Route*/
+  Route::resource('product',ProductController::class );
 });
