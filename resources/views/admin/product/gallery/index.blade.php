52193e68c27dfd96c249f4b4ea75f1e8004f6184
Phucle2705 <0306201275@caothang.edu.vn>
2024-04-03 10:30:47 +0700

    Product Gallery - Working with Create Feature

--- resources/views/admin/product/gallery/index.blade.php
+++ resources/views/admin/product/gallery/index.blade.php
@@ -3,20 +3,27 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>Sản Phẩm</h1>
+            <h1>Sản Phẩm Trưng bày </h1>
         </div>
 
         <div class="card card-primary">
             <div class="card-header">
-                <h4>Tất cả sản phẩm</h4>
-                <div class="card-header-action">
-                    <a href="{{ route('admin.product.create') }}" class="btn btn-primary">
-                        Thêm mới
-                    </a>
-                </div>
+                <h4>Tất cả hình ảnh sản phẩm</h4>
+
             </div>
             <div class="card-body">
-
+                 <div class="col-md-8">
+                   <form action="{{ route('admin.product-gallery.store') }}" method="POST" enctype="multipart/form-data">
+                   @csrf
+                    <div class="form-group">
+                        <input type="file" class="form-control" name="image">
+                        <input type="hidden" value="{{ $productId }}" name="product_id">
+                    </div>
+                    <div class="form-group">
+                       <button type="submit" class="btn btn-primary">Tải lên</button>
+                    </div>
+                   </form>
+                 </div>
             </div>
         </div>
     </section>
--- storage/framework/views/987a90c0abc90fe85ce4e0b2c09c9127.php
+++ storage/framework/views/987a90c0abc90fe85ce4e0b2c09c9127.php
@@ -1,20 +1,27 @@
 <?php $__env->startSection('content'); ?>
     <section class="section">
         <div class="section-header">
-            <h1>Sản Phẩm</h1>
+            <h1>Sản Phẩm Trưng bày </h1>
         </div>
 
         <div class="card card-primary">
             <div class="card-header">
-                <h4>Tất cả sản phẩm</h4>
-                <div class="card-header-action">
-                    <a href="<?php echo e(route('admin.product.create')); ?>" class="btn btn-primary">
-                        Thêm mới
-                    </a>
-                </div>
+                <h4>Tất cả hình ảnh sản phẩm</h4>
+
             </div>
             <div class="card-body">
-
+                 <div class="col-md-8">
+                   <form action="<?php echo e(route('admin.product-gallery.store')); ?>" method="POST" enctype="multipart/form-data">
+                   <?php echo csrf_field(); ?>
+                    <div class="form-group">
+                        <input type="file" class="form-control" name="image">
+                        <input type="hidden" value="<?php echo e($productId); ?>" name="product_id">
+                    </div>
+                    <div class="form-group">
+                       <button type="submit" class="btn btn-primary">Tải lên</button>
+                    </div>
+                   </form>
+                 </div>
             </div>
         </div>
     </section>
