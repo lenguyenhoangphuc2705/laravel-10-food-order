c66f49d5f1bbd9617812e68efc780ffd5a062320
Phucle2705 <0306201275@caothang.edu.vn>
2024-04-04 12:11:31 +0700

    Showing Products - Show Dynamic Product in Food Menu Section (Part - 2)

--- storage/framework/views/97274eaf6484537c5d3b8fe59cd4ed3d.php
+++ storage/framework/views/97274eaf6484537c5d3b8fe59cd4ed3d.php
@@ -27,34 +27,55 @@
         </div>
 
         <div class="row grid">
+       <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+       <?php
+           $products = \App\Models\Product::where(['show_at_home'=> 1, 'status'=> 1, 'category_id'=>$category->id])
+           ->orderBy('id','DESC')
+           ->take(8)
+           ->get();
+       ?>
 
-            <div class="col-xl-3 col-sm-6 col-lg-4  chicken dresserts wow fadeInUp" data-wow-duration="1s">
-                <div class="fp__menu_item">
-                    <div class="fp__menu_item_img">
-                        <img src="images/menu2_img_2.jpg" alt="menu" class="img-fluid w-100">
-                        <a class="category" href="#">chicken</a>
-                    </div>
-                    <div class="fp__menu_item_text">
-                        <p class="rating">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star-half-alt"></i>
-                            <i class="far fa-star"></i>
-                            <span>145</span>
-                        </p>
-                        <a class="title" href="menu_details.html">chicken Masala</a>
-                        <h5 class="price">$80.00 <del>90.00</del></h5>
-                        <ul class="d-flex flex-wrap justify-content-center">
-                            <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
-                                        class="fas fa-shopping-basket"></i></a></li>
-                            <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                            <li><a href="#"><i class="far fa-eye"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
+       <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+       <div class="col-xl-3 col-sm-6 col-lg-4 <?php echo e($category->slug); ?> wow fadeInUp" data-wow-duration="1s">
+        <div class="fp__menu_item">
+            <div class="fp__menu_item_img">
+                <img src="<?php echo e(asset($product->thumb_image)); ?>" alt="<?php echo e($product->name); ?>" class="img-fluid w-100">
+                <a class="category" href="#"><?php echo e($product->category->name); ?></a>
             </div>
-           
+            <div class="fp__menu_item_text">
+                <p class="rating">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                    <i class="far fa-star"></i>
+                    <span>145</span>
+                </p>
+                <a class="title" href="menu_details.html"><?php echo e($product->name); ?></a>
+                <h5 class="price">
+
+                    <?php if($product->offer_price >0): ?>
+                    $<?php echo e($product->offer_price); ?>
+
+                    <del>$<?php echo e($product->price); ?></del>
+                    <?php else: ?>
+                    $<?php echo e($product->price); ?>
+
+                    <?php endif; ?>
+                </h5>
+                <ul class="d-flex flex-wrap justify-content-center">
+                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
+                                class="fas fa-shopping-basket"></i></a></li>
+                    <li><a href="#"><i class="fal fa-heart"></i></a></li>
+                    <li><a href="#"><i class="far fa-eye"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+       <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+       <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
         </div>
     </div>
 </section>
