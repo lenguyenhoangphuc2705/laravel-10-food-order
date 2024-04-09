414e9d9e0f317f52b9c40d8a889faf081afc0226
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2024-04-09 22:01:28 +0700

    Cart Feature - Working with Product Popup Modal (Part - 3)

--- storage/framework/views/f3bfdd2fd2e450bdbeb9c1a83f3512bb.php
+++ storage/framework/views/f3bfdd2fd2e450bdbeb9c1a83f3512bb.php
@@ -1,11 +1,12 @@
 <script>
     //Load product modal 
     function loadProductModal(productId){
-        s.ajax({
+        $.ajax({
             method: 'GET',
             url: '<?php echo e(route("load-product-modal", ":productId")); ?>'.replace(':productId', productId) ,
             success: function(response){
-               
+               $(".load_product_modal_body").html(response);
+               $('#cartModal').modal('show');
             },
             error: function(xhr, status, error){
                 console.error(error);
