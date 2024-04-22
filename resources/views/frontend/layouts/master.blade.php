99371697a76efb34e489b0fcb8aded436ee2b450
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2024-04-22 23:37:07 +0700

    Cart Feature - Adding Loader on Ajax Request

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -21,10 +21,18 @@
     <link rel="stylesheet" href="{{ asset('frontend/css/toastr.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
+
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
 </head>
 
 <body>
+    <div class="overlay-container">
+        <div class="overlay">
+            <span class="loader"></span>
+        </div>
+    </div>
 
     <!--=============================
         TOPBAR START
