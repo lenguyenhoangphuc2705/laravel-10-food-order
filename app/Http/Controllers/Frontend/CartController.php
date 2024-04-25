2e996b3c53fdf4cb8a7dc9867106a28e4644acbb
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2024-04-25 23:48:24 +0700

    Add to Cart - Working with Add to Cart Feature (Part - 3)

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -15,14 +16,19 @@ class CartController extends Controller
         $productSize = $product->productSizes->where('id', $request->product_size)->first();
         $productOptions = $product->productOptions->whereIn('id',$request->product_option);
         
-        $option = [
+        $options = [
             'product_size'=> [
-                'id' => $productSize->id,
-                'name' => $productSize->name,
-                'price' => $productSize->price
+                'id' => $productSize?->id,
+                'name' => $productSize?->name,
+                'price' => $productSize?->price
 
             ],
             'product_option' => [],
+            'product_info' => [
+                'image' => $product->thumb_image,
+                'slug' => $product->slug,
+
+            ]
         ];
 
         foreach($productOptions as $option){
@@ -32,5 +38,17 @@ class CartController extends Controller
                 'price' => $option->price
             ];
         }
+
+        Cart::add([
+            'id' => $product->id,
+            'name' => $product->name,
+            'qty' => $request->quantity,
+            'price' => $product->offer_price > 0 ? $product->offer_price : $product->offer_price,
+            'weight' => 0,
+            'options' => $options,
+        ]);
+
+        return response(['status' => 'success', 'message' => 'Product added into cart'], 200);
+
     }   
 }
