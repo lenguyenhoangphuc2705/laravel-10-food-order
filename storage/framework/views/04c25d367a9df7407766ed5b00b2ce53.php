04d54664857d6740317c733d8e0061296519efa8
lehoangminh23112001 <lehoangminh23112001adn@gmail.com>
2024-04-27 23:35:09 +0700

    Sidebar Cart - Showing Dynamic Cart Products at Sidebar

--- storage/framework/views/04c25d367a9df7407766ed5b00b2ce53.php
+++ storage/framework/views/04c25d367a9df7407766ed5b00b2ce53.php
@@ -80,69 +80,25 @@
             <span class="close_cart"><i class="fal fa-times"></i></span>
         </div>
         <ul>
+            <?php $__currentLoopData = Cart::content(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $cartProduct): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <li>
                 <div class="menu_cart_img">
-                    <img src="images/menu8.png" alt="menu" class="img-fluid w-100">
+                    <img src="<?php echo e(asset($cartProduct->options->product_info['image'])); ?>" alt="menu" class="img-fluid w-100">
                 </div>
                 <div class="menu_cart_text">
-                    <a class="title" href="#">Hyderabadi Biryani </a>
-                    <p class="size">small</p>
-                    <span class="extra">coca-cola</span>
-                    <span class="extra">7up</span>
-                    <p class="price">$99.00 <del>$110.00</del></p>
+                    <a class="title" href=<?php echo e(route('product.show', $cartProduct->options->product_info['slug'])); ?>"><?php echo $cartProduct->name; ?> </a>
+                    <p class="size">Qty: <?php echo e($cartProduct->qty); ?></p>
+                    <p class="size"><?php echo e(@$cartProduct->options->product_size['name']); ?></p>
+                    
+                    <p class="price"><?php echo e(currencyPosition($cartProduct->price)); ?></p>
                 </div>
                 <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu4.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Chicken Masalas</a>
-                    <p class="size">medium</p>
-                    <span class="extra">7up</span>
-                    <p class="price">$70.00</p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu5.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Competently Supply Customized Initiatives</a>
-                    <p class="size">large</p>
-                    <span class="extra">coca-cola</span>
-                    <span class="extra">7up</span>
-                    <p class="price">$120.00 <del>$150.00</del></p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu6.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Hyderabadi Biryani</a>
-                    <p class="size">small</p>
-                    <span class="extra">7up</span>
-                    <p class="price">$59.00</p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu1.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Competently Supply</a>
-                    <p class="size">medium</p>
-                    <span class="extra">coca-cola</span>
-                    <span class="extra">7up</span>
-                    <p class="price">$99.00 <del>$110.00</del></p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
+            </li> 
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            
+
+            
+            
         </ul>
         <p class="subtotal">sub total <span>$1220.00</span></p>
         <a class="cart_view" href="cart_view.html"> view cart</a>
